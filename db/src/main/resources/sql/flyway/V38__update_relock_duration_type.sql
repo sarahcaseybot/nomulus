1c50b72157563d4c1d0bbef441fed60cbebc8564
Sarah Botwinick <sarahbot@google.com>
2020-07-01 16:09:53 -0400

    Change column type for relock_duration

--- db/src/main/resources/sql/flyway/V38__update_relock_duration_type.sql
+++ db/src/main/resources/sql/flyway/V38__update_relock_duration_type.sql
@@ -1,4 +1,4 @@
- -- Copyright 2020 The Nomulus Authors. All Rights Reserved.
+-- Copyright 2020 The Nomulus Authors. All Rights Reserved.
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
@@ -12,4 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-ALTER TABLE "RegistryLock" ADD COLUMN relock_duration bigint;
+ALTER TABLE "RegistryLock" DROP COLUMN relock_duration;
+
+ALTER TABLE "RegistryLock" ADD COLUMN relock_duration interval;
+
