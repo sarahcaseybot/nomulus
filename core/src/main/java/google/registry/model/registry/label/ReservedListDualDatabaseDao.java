a1b56232a04857374027bf199dfeeadfa325f658
Sarah Botwinick <sarahbot@google.com>
2021-03-19 16:43:02 -0400

    fix typo

--- core/src/main/java/google/registry/model/registry/label/ReservedListDualDatabaseDao.java
+++ core/src/main/java/google/registry/model/registry/label/ReservedListDualDatabaseDao.java
@@ -51,7 +51,7 @@ public class ReservedListDualDatabaseDao {
     }
   }
 
-  /** Delete a reserved list from from databases. */
+  /** Delete a reserved list from both databases. */
   public static void delete(ReservedList reservedList) {
     if (isDatastore(TransitionId.DOMAIN_LABEL_LISTS)) {
       ReservedListDatastoreDao.delete(reservedList);
