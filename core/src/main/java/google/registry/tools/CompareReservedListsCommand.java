a1a47d569e0d393fb0b8e299c64df17931231107
Sarah Botwinick <sarahbot@google.com>
2021-04-02 15:43:33 -0400

    Add a CompareReservedListCommand

---
+++ core/src/main/java/google/registry/tools/CompareReservedListsCommand.java
@@ -0,0 +1,112 @@
+// Copyright 2021 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package google.registry.tools;
+
+import static com.google.common.collect.ImmutableSortedSet.toImmutableSortedSet;
+import static google.registry.model.common.EntityGroupRoot.getCrossTldKey;
+import static google.registry.model.ofy.ObjectifyService.ofy;
+import static google.registry.persistence.transaction.TransactionManagerFactory.jpaTm;
+
+import com.beust.jcommander.Parameters;
+import com.google.appengine.repackaged.com.google.common.collect.Streams;
+import com.google.common.collect.ImmutableSet;
+import google.registry.model.registry.label.ReservedList;
+import google.registry.model.registry.label.ReservedList.ReservedListEntry;
+import google.registry.model.registry.label.ReservedListDatastoreDao;
+import google.registry.model.registry.label.ReservedListSqlDao;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/** Command to compare all ReservedLists in Datastore to all ReservedLists in Cloud SQL. */
+@Parameters(
+    separators = " =",
+    commandDescription = "Compare all the ReservedLists in Datastore to those in Cloud SQL.")
+final class CompareReservedListsCommand implements CommandWithRemoteApi {
+
+  @Override
+  public void run() {
+    ImmutableSet<ReservedList> datastoreLists =
+        ImmutableSet.copyOf(
+            ofy().load().type(ReservedList.class).ancestor(getCrossTldKey()).list());
+
+    ImmutableSet<ReservedList> cloudSqlLists =
+        jpaTm()
+            .transact(
+                () ->
+                    jpaTm().loadAllOf(ReservedList.class).stream()
+                        .map(ReservedList::getName)
+                        .map(ReservedListSqlDao::getLatestRevision)
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .collect(
+                            toImmutableSortedSet(Comparator.comparing(ReservedList::getName))));
+
+    int listsWithDiffs = 0;
+
+    for (ReservedList datastoreList : datastoreLists) {
+      Optional<ReservedList> sqlList =
+          ReservedListSqlDao.getLatestRevision(datastoreList.getName());
+      if (!sqlList.isPresent()) {
+        listsWithDiffs++;
+        System.out.printf(
+            "ReservedList with name %s is present in Datastore, but not in Cloud SQL%n",
+            datastoreList.getName());
+      } else {
+        String datastoreListString =
+            Streams.stream(
+                    ReservedListDatastoreDao.getLatestRevision(datastoreList.getName())
+                        .get()
+                        .getReservedListEntries()
+                        .values())
+                .sorted(Comparator.comparing(ReservedListEntry::getLabel))
+                .map(ReservedListEntry::toString)
+                .collect(Collectors.joining("\n"));
+
+        String sqlListString =
+            Streams.stream(
+                    ReservedListSqlDao.getLatestRevision(datastoreList.getName())
+                        .get()
+                        .getReservedListEntries()
+                        .values())
+                .sorted(Comparator.comparing(ReservedListEntry::getLabel))
+                .map(ReservedListEntry::toString)
+                .collect(Collectors.joining("\n"));
+
+        // This will only print out the name of the unequal list. GetReservedListCommand should be
+        // used to determine what the actual differences are.
+        if (!datastoreListString.equals(sqlListString)) {
+          listsWithDiffs++;
+          System.out.printf(
+              "ReservedList with name %s has different entries in each database%n",
+              datastoreList.getName());
+        }
+      }
+    }
+
+    for (ReservedList sqlList : cloudSqlLists) {
+      Optional<ReservedList> datastoreList =
+          ReservedListDatastoreDao.getLatestRevision(sqlList.getName());
+      if (!datastoreList.isPresent()) {
+        listsWithDiffs++;
+        System.out.printf(
+            "ReservedList with name %s is present in Cloud SQL, but not in Datastore%n",
+            sqlList.getName());
+      }
+    }
+
+    System.out.printf("Found %s unequal lists.%n", listsWithDiffs);
+  }
+}
---
+++ core/src/test/java/google/registry/tools/CompareReservedListCommandTest.java
@@ -0,0 +1,146 @@
+// Copyright 2021 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package google.registry.tools;
+
+import static com.google.common.truth.Truth.assertThat;
+import static google.registry.persistence.transaction.TransactionManagerFactory.jpaTm;
+import static google.registry.persistence.transaction.TransactionManagerFactory.ofyTm;
+
+import com.google.common.collect.ImmutableMap;
+import google.registry.model.registry.label.ReservationType;
+import google.registry.model.registry.label.ReservedList;
+import google.registry.model.registry.label.ReservedList.ReservedListEntry;
+import google.registry.model.registry.label.ReservedListDatastoreDao;
+import google.registry.model.registry.label.ReservedListDualDatabaseDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/** Unit tests for {@link CompareReservedListsCommand}. */
+public class CompareReservedListCommandTest extends CommandTestCase<CompareReservedListsCommand> {
+
+  private ImmutableMap<String, ReservedListEntry> reservations;
+  private ImmutableMap<String, ReservedListEntry> reservations2;
+
+  private ReservedList reservedList;
+  private ReservedList reservedList2;
+
+  @BeforeEach
+  void setUp() {
+    reservations =
+        ImmutableMap.of(
+            "food",
+            ReservedListEntry.create("food", ReservationType.RESERVED_FOR_SPECIFIC_USE, null),
+            "music",
+            ReservedListEntry.create("music", ReservationType.FULLY_BLOCKED, "fully blocked"));
+
+    reservations2 =
+        ImmutableMap.of(
+            "candy", ReservedListEntry.create("candy", ReservationType.ALLOWED_IN_SUNRISE, null));
+
+    reservedList =
+        new ReservedList.Builder()
+            .setName("testlist")
+            .setLastUpdateTime(fakeClock.nowUtc())
+            .setShouldPublish(false)
+            .setReservedListMap(reservations)
+            .build();
+
+    reservedList2 =
+        new ReservedList.Builder()
+            .setName("testlist2")
+            .setLastUpdateTime(fakeClock.nowUtc())
+            .setShouldPublish(false)
+            .setReservedListMap(reservations2)
+            .build();
+
+    ReservedListDualDatabaseDao.save(reservedList);
+    ReservedListDualDatabaseDao.save(reservedList2);
+  }
+
+  @Test
+  void test_success() throws Exception {
+    runCommand();
+    String stdout = getStdoutAsString();
+    assertThat(stdout).isEqualTo("Found 0 unequal lists.\n");
+  }
+
+  @Test
+  void test_listMissingFromCloudSql() throws Exception {
+    jpaTm().transact(() -> jpaTm().delete(reservedList));
+    runCommand();
+    String stdout = getStdoutAsString();
+    assertThat(stdout)
+        .isEqualTo(
+            "ReservedList with name testlist is present in Datastore, but not in Cloud SQL\n"
+                + "Found 1 unequal lists.\n");
+  }
+
+  @Test
+  void test_listMissingFromDatastore() throws Exception {
+    ofyTm().transact(() -> ofyTm().delete(reservedList));
+    runCommand();
+    String stdout = getStdoutAsString();
+    assertThat(stdout)
+        .isEqualTo(
+            "ReservedList with name testlist is present in Cloud SQL, but not in Datastore\n"
+                + "Found 1 unequal lists.\n");
+  }
+
+  @Test
+  void test_listsDiffer() throws Exception {
+    ImmutableMap<String, ReservedListEntry> newReservations =
+        ImmutableMap.of(
+            "food",
+            ReservedListEntry.create("food", ReservationType.RESERVED_FOR_SPECIFIC_USE, null));
+    ReservedList secondList =
+        new ReservedList.Builder()
+            .setName("testlist")
+            .setLastUpdateTime(fakeClock.nowUtc())
+            .setShouldPublish(false)
+            .setReservedListMap(newReservations)
+            .build();
+    ReservedListDatastoreDao.save(secondList);
+    runCommand();
+    String stdout = getStdoutAsString();
+    assertThat(stdout)
+        .isEqualTo(
+            "ReservedList with name testlist has different entries in each database\n"
+                + "Found 1 unequal lists.\n");
+  }
+
+  @Test
+  void test_listsDifferAndMissing() throws Exception {
+    ofyTm().transact(() -> ofyTm().delete(reservedList2));
+    ImmutableMap<String, ReservedListEntry> newReservations =
+        ImmutableMap.of(
+            "food",
+            ReservedListEntry.create("food", ReservationType.RESERVED_FOR_SPECIFIC_USE, null));
+    ReservedList secondList =
+        new ReservedList.Builder()
+            .setName("testlist")
+            .setLastUpdateTime(fakeClock.nowUtc())
+            .setShouldPublish(false)
+            .setReservedListMap(newReservations)
+            .build();
+    ReservedListDatastoreDao.save(secondList);
+    runCommand();
+    String stdout = getStdoutAsString();
+    assertThat(stdout)
+        .isEqualTo(
+            "ReservedList with name testlist has different entries in each database\n"
+                + "ReservedList with name testlist2 is present in Cloud SQL, but not in Datastore\n"
+                + "Found 2 unequal lists.\n");
+  }
+}
