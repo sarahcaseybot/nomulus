832b2823acaf9a7cf3a023331a0acff4ff4221aa
Sarah Botwinick <sarahbot@google.com>
2021-05-17 13:05:30 -0400

    Fix NullPointerException in StaticPremiumPricingEngine

--- core/src/main/java/google/registry/model/pricing/StaticPremiumListPricingEngine.java
+++ core/src/main/java/google/registry/model/pricing/StaticPremiumListPricingEngine.java
@@ -38,8 +38,10 @@ public final class StaticPremiumListPricingEngine implements PremiumPricingEngin
     String tld = getTldFromDomainName(fullyQualifiedDomainName);
     String label = InternetDomainName.from(fullyQualifiedDomainName).parts().get(0);
     Registry registry = Registry.get(checkNotNull(tld, "tld"));
-    Optional<Money> premiumPrice =
-        PremiumListDao.getPremiumPrice(registry.getPremiumList().getName(), label);
+    Optional<Money> premiumPrice = Optional.empty();
+    if (registry.getPremiumList() != null) {
+      premiumPrice = PremiumListDao.getPremiumPrice(registry.getPremiumList().getName(), label);
+    }
     return DomainPrices.create(
         premiumPrice.isPresent(),
         premiumPrice.orElse(registry.getStandardCreateCost()),
